f841464679a5a292136579773b1659460fe6ace4
Salvador Guzman <guzmansalv@gmail.com>
2018-03-23 22:29:05 -0700

    Update shows.js

--- shows.js
+++ shows.js
@@ -1,54 +1,65 @@
-const shows = { Citrus: 
-   { title: 'Citrus',
-     img: 'https://www.watchcartoononline.io/wp-content/catimg/584022.jpg',
-     desc: 'Fashionable Yuzu imagined the first day at her new school she transferred to after her mother&#x2019;s remarriage a bit differently; she didn&#x2019;t know it&#x2019;d be an ultra-strict and conservative girls&#x2019; school. So instead of a sweet high-school romance, she butts heads with the stern student council president Mei&#x2014;who turns out to be her new stepsister. And she has to learn that hate and attraction are often not that far apart.',
-     genres: [ 'Drama', 'Romance', 'school', 'Shoujo Ai' ],
-     episodes: 
-      { '1': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%201.mp4?st=W2B3_wlKnHCg2kiP9abkAA&e=1521869802',
-        '2': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%202.mp4?st=B5kvOBaOvhpluLoDRqjf3Q&e=1521869802',
-        '3': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/citrus3hd.mp4?st=kdAG4d5AHVUrdGXb0Qx2qw&e=1521869802',
-        '4': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus.mp4?st=ukaTTvZVO5xRxJ5lXHmNvA&e=1521869802',
-        '5': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%205.mp4?st=ns55Bdgvirh2Nm-e1vx-fw&e=1521869802',
-        '6': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/citrus%206.mp4?st=PXFxI80oZ-VAycByBBldRg&e=1521869802',
-        '7': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%207.mp4?st=DOm9RL9ApHCBoRsx9SSy5A&e=1521869801',
-        '8': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%208.mp4?st=l_eiHFKyuiXkLWqrwPFuhA&e=1521869801',
-        '9': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%209.mp4?st=zqla1eXuK6v_hpdYc0Yd6Q&e=1521869802',
-        '10': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%2010.mp4?st=wRsFpU325Ns461ZS2LM7-Q&e=1521869801',
-        '11': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%2011.mp4?st=ofDO5y-nApbZ3QV84UpuZw&e=1521869802' } },
-  'Aku no Hana': 
-   { title: 'Aku no Hana',
-     img: 'https://www.watchcartoononline.io/wp-content/catimg/587128.jpg',
-     desc: 'Kasuga Takao is a boy who loves reading books, particularly Baudelaire&#x2019;s Les Fleurs du Mal. A girl at his school, Saeki Nanako, is his muse and his Venus, and he admires her from a distance. One day, he forgets his copy of Les Fleurs du Mal in the classroom and runs back alone to pick it up. In the classroom, he finds not only his book, but Saeki&#x2019;s gym uniform. On a mad impulse, he steals it.',
-     genres: [ 'Drama', 'Psychological', 'Romance', 'Scholl' ],
-     episodes: 
-      { '1': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20001%20-%20Watch%20Aku%20no%20Hana%20Episode%20001%20online%20in%20high%20quality.mp4?st=X2fTHPN9JC4MzVerpf3zAQ&e=1521869802',
-        '2': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20002%20-%20Watch%20Aku%20no%20Hana%20Episode%20002%20online%20in%20high%20quality.mp4?st=KpKBmJQmL1r-kSFGnwGafQ&e=1521869802',
-        '3': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20003%20-%20Watch%20Aku%20no%20Hana%20Episode%20003%20online%20in%20high%20quality.mp4?st=29XEupu47qrYL2QoYmrqtQ&e=1521869802',
-        '4': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20004%20-%20Watch%20Aku%20no%20Hana%20Episode%20004%20online%20in%20high%20quality.mp4?st=icldiu_Ua_cjRwMWGC7Tog&e=1521869802',
-        '5': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20005%20-%20Watch%20Aku%20no%20Hana%20Episode%20005%20online%20in%20high%20quality.mp4?st=xxUeOnCNmDC7bBCnNYTjJQ&e=1521869802',
-        '6': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20006%20-%20Watch%20Aku%20no%20Hana%20Episode%20006%20online%20in%20high%20quality.mp4?st=-JHWqwxHUEHwoGgIyJkvLQ&e=1521869801',
-        '7': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20007%20-%20Watch%20Aku%20no%20Hana%20Episode%20007%20online%20in%20high%20quality.mp4?st=gbG7bzWvItFa2ZKdjf2-pA&e=1521869802',
-        '8': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20008%20-%20Watch%20Aku%20no%20Hana%20Episode%20008%20online%20in%20high%20quality.mp4?st=-fjCTweg7Dt035mx23C93w&e=1521869802',
-        '9': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20009%20-%20Watch%20Aku%20no%20Hana%20Episode%20009%20online%20in%20high%20quality.mp4?st=gOw5LJLvXYNS4KSvfwr4vQ&e=1521869802',
-        '10': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20010%20-%20Watch%20Aku%20no%20Hana%20Episode%20010%20online%20in%20high%20quality.mp4?st=G1GPH-BxivEmYtDAHktYAQ&e=1521869802',
-        '11': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20011%20-%20Watch%20Aku%20no%20Hana%20Episode%20011%20online%20in%20high%20quality.mp4?st=_c5bvaOPgeWGyE-WpJz1cw&e=1521869802',
-        '12': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20012%20-%20Watch%20Aku%20no%20Hana%20Episode%20012%20online%20in%20high%20quality.mp4?st=q70-fAU6ZkKFJOAi5872Eg&e=1521869802',
-        '13': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20013%20-%20Watch%20Aku%20no%20Hana%20Episode%20013%20online%20in%20high%20quality.mp4?st=E-hDP5U_8r1Hw9J-gIMmjA&e=1521869802' } },
-  'A Channel': 
-   { title: 'A Channel',
-     img: 'https://www.watchcartoononline.io/wp-content/catimg/580884.jpg',
-     desc: 'Tooru and Run have been best friends forever, so when Tooru learns that she&#x2019;s managed to get into the same high school as Run, she runs to tell her&#x2026; only to find Run in a compromising position with yet another girl, Yuko. Needless to say, that makes things a bit awkward at school, with Tooru fending off those who might be interested in Run while Yuko and their other girlfriend, Nagi, have to deal with Run&#x2019;s own penchant for drama.',
-     genres: [ 'Comedy', 'Scholl', 'Sci-fi' ],
-     episodes: 
-      { '1': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20001%20-%20Watch%20A%20Channel%20_Special%20001%20online%20in%20high%20quality.mp4?st=fGjuJxEVVETh_W6GIxeBkg&e=1521869802',
-        '2': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20002%20-%20Watch%20A%20Channel%20Episode%20002%20online%20in%20high%20quality.mp4?st=cqeJb9LAoFHHRJ2srU1p7A&e=1521869802',
-        '3': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20003%20-%20Watch%20A%20Channel%20_Special%20003%20online%20in%20high%20quality.mp4?st=8SwWONcC_6PejjgKeruYPA&e=1521869802',
-        '4': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20004%20-%20Watch%20A%20Channel%20Episode%20004%20online%20in%20high%20quality.mp4?st=JXbggfDUnY1cbKSdCZ8Hlg&e=1521869802',
-        '5': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20005%20-%20Watch%20A%20Channel%20_Special%20005%20online%20in%20high%20quality.mp4?st=5N6RZKZgGxsI4CAMAltVyA&e=1521869802',
-        '6': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20006%20-%20Watch%20A%20Channel%20Episode%20006%20online%20in%20high%20quality.mp4?st=J2j091yLXzhk1ETS89kOlw&e=1521869802',
-        '7': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20007%20-%20Watch%20A%20Channel%20Episode%20007%20online%20in%20high%20quality.mp4?st=JIgYwhtdUGPl-Db039p-Gg&e=1521869802',
-        '8': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20008%20-%20Watch%20A%20Channel%20_Special%20008%20online%20in%20high%20quality.mp4?st=vapQu1DhWcD4ql9GgSxbsQ&e=1521869802',
-        '9': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20009%20-%20Watch%20A%20Channel%20Episode%20009%20online%20in%20high%20quality.mp4?st=9E7shB1Y3xjAnJFDA_HgrQ&e=1521869802',
-        '10': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20010%20-%20Watch%20A%20Channel%20_Special%20010%20online%20in%20high%20quality.mp4?st=LSanrMHrrtubUIelCf0dLg&e=1521869802',
-        '11': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20011%20-%20Watch%20A%20Channel%20_Special%20011%20online%20in%20high%20quality.mp4?st=MfnhP8sR_6vDYkDsQ0CUgA&e=1521869802',
-        '12': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20012%20-%20Watch%20A%20Channel%20Episode%20012%20online%20in%20high%20quality.mp4?st=rwb7KmKecwZCyohA51uZFA&e=1521869802' } } }
+const shows = [
+      {
+        title: 'Citrus',
+        img: 'https://www.watchcartoononline.io/wp-content/catimg/584022.jpg',
+        desc: 'Fashionable Yuzu imagined the first day at her new school she transferred to after her mother&#x2019;s remarriage a bit differently; she didn&#x2019;t know it&#x2019;d be an ultra-strict and conservative girls&#x2019; school. So instead of a sweet high-school romance, she butts heads with the stern student council president Mei&#x2014;who turns out to be her new stepsister. And she has to learn that hate and attraction are often not that far apart.',
+        genres: ['Drama', 'Romance', 'school', 'Shoujo Ai'],
+        episodes:
+          {
+            '1': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%201.mp4?st=W2B3_wlKnHCg2kiP9abkAA&e=1521869802',
+            '2': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%202.mp4?st=B5kvOBaOvhpluLoDRqjf3Q&e=1521869802',
+            '3': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/citrus3hd.mp4?st=kdAG4d5AHVUrdGXb0Qx2qw&e=1521869802',
+            '4': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus.mp4?st=ukaTTvZVO5xRxJ5lXHmNvA&e=1521869802',
+            '5': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/Citrus%20Episode%205.mp4?st=ns55Bdgvirh2Nm-e1vx-fw&e=1521869802',
+            '6': 'https://cdn.cizgifilmlerizle.com/cizgi/animesw/citrus%206.mp4?st=PXFxI80oZ-VAycByBBldRg&e=1521869802',
+            '7': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%207.mp4?st=DOm9RL9ApHCBoRsx9SSy5A&e=1521869801',
+            '8': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%208.mp4?st=l_eiHFKyuiXkLWqrwPFuhA&e=1521869801',
+            '9': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%209.mp4?st=zqla1eXuK6v_hpdYc0Yd6Q&e=1521869802',
+            '10': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%2010.mp4?st=wRsFpU325Ns461ZS2LM7-Q&e=1521869801',
+            '11': 'https://cdn.cizgifilmlerizle.com/cizgi/Citrus%20Episode%2011.mp4?st=ofDO5y-nApbZ3QV84UpuZw&e=1521869802'
+          }
+      },
+        {
+          title: 'Aku no Hana',
+          img: 'https://www.watchcartoononline.io/wp-content/catimg/587128.jpg',
+          desc: 'Kasuga Takao is a boy who loves reading books, particularly Baudelaire&#x2019;s Les Fleurs du Mal. A girl at his school, Saeki Nanako, is his muse and his Venus, and he admires her from a distance. One day, he forgets his copy of Les Fleurs du Mal in the classroom and runs back alone to pick it up. In the classroom, he finds not only his book, but Saeki&#x2019;s gym uniform. On a mad impulse, he steals it.',
+          genres: ['Drama', 'Psychological', 'Romance', 'Scholl'],
+          episodes:
+            {
+              '1': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20001%20-%20Watch%20Aku%20no%20Hana%20Episode%20001%20online%20in%20high%20quality.mp4?st=X2fTHPN9JC4MzVerpf3zAQ&e=1521869802',
+              '2': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20002%20-%20Watch%20Aku%20no%20Hana%20Episode%20002%20online%20in%20high%20quality.mp4?st=KpKBmJQmL1r-kSFGnwGafQ&e=1521869802',
+              '3': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20003%20-%20Watch%20Aku%20no%20Hana%20Episode%20003%20online%20in%20high%20quality.mp4?st=29XEupu47qrYL2QoYmrqtQ&e=1521869802',
+              '4': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20004%20-%20Watch%20Aku%20no%20Hana%20Episode%20004%20online%20in%20high%20quality.mp4?st=icldiu_Ua_cjRwMWGC7Tog&e=1521869802',
+              '5': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20005%20-%20Watch%20Aku%20no%20Hana%20Episode%20005%20online%20in%20high%20quality.mp4?st=xxUeOnCNmDC7bBCnNYTjJQ&e=1521869802',
+              '6': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20006%20-%20Watch%20Aku%20no%20Hana%20Episode%20006%20online%20in%20high%20quality.mp4?st=-JHWqwxHUEHwoGgIyJkvLQ&e=1521869801',
+              '7': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20007%20-%20Watch%20Aku%20no%20Hana%20Episode%20007%20online%20in%20high%20quality.mp4?st=gbG7bzWvItFa2ZKdjf2-pA&e=1521869802',
+              '8': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20008%20-%20Watch%20Aku%20no%20Hana%20Episode%20008%20online%20in%20high%20quality.mp4?st=-fjCTweg7Dt035mx23C93w&e=1521869802',
+              '9': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20009%20-%20Watch%20Aku%20no%20Hana%20Episode%20009%20online%20in%20high%20quality.mp4?st=gOw5LJLvXYNS4KSvfwr4vQ&e=1521869802',
+              '10': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20010%20-%20Watch%20Aku%20no%20Hana%20Episode%20010%20online%20in%20high%20quality.mp4?st=G1GPH-BxivEmYtDAHktYAQ&e=1521869802',
+              '11': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20011%20-%20Watch%20Aku%20no%20Hana%20Episode%20011%20online%20in%20high%20quality.mp4?st=_c5bvaOPgeWGyE-WpJz1cw&e=1521869802',
+              '12': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20012%20-%20Watch%20Aku%20no%20Hana%20Episode%20012%20online%20in%20high%20quality.mp4?st=q70-fAU6ZkKFJOAi5872Eg&e=1521869802',
+              '13': 'https://lb.watchanimesub.net/Aku%20no%20Hana/Aku%20no%20Hana%20Episode%20013%20-%20Watch%20Aku%20no%20Hana%20Episode%20013%20online%20in%20high%20quality.mp4?st=E-hDP5U_8r1Hw9J-gIMmjA&e=1521869802'
+            }
+        },
+        {
+          title: 'A Channel',
+          img: 'https://www.watchcartoononline.io/wp-content/catimg/580884.jpg',
+          desc: 'Tooru and Run have been best friends forever, so when Tooru learns that she&#x2019;s managed to get into the same high school as Run, she runs to tell her&#x2026; only to find Run in a compromising position with yet another girl, Yuko. Needless to say, that makes things a bit awkward at school, with Tooru fending off those who might be interested in Run while Yuko and their other girlfriend, Nagi, have to deal with Run&#x2019;s own penchant for drama.',
+          genres: ['Comedy', 'Scholl', 'Sci-fi'],
+          episodes:
+            {
+              '1': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20001%20-%20Watch%20A%20Channel%20_Special%20001%20online%20in%20high%20quality.mp4?st=fGjuJxEVVETh_W6GIxeBkg&e=1521869802',
+              '2': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20002%20-%20Watch%20A%20Channel%20Episode%20002%20online%20in%20high%20quality.mp4?st=cqeJb9LAoFHHRJ2srU1p7A&e=1521869802',
+              '3': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20003%20-%20Watch%20A%20Channel%20_Special%20003%20online%20in%20high%20quality.mp4?st=8SwWONcC_6PejjgKeruYPA&e=1521869802',
+              '4': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20004%20-%20Watch%20A%20Channel%20Episode%20004%20online%20in%20high%20quality.mp4?st=JXbggfDUnY1cbKSdCZ8Hlg&e=1521869802',
+              '5': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20005%20-%20Watch%20A%20Channel%20_Special%20005%20online%20in%20high%20quality.mp4?st=5N6RZKZgGxsI4CAMAltVyA&e=1521869802',
+              '6': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20006%20-%20Watch%20A%20Channel%20Episode%20006%20online%20in%20high%20quality.mp4?st=J2j091yLXzhk1ETS89kOlw&e=1521869802',
+              '7': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20007%20-%20Watch%20A%20Channel%20Episode%20007%20online%20in%20high%20quality.mp4?st=JIgYwhtdUGPl-Db039p-Gg&e=1521869802',
+              '8': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20008%20-%20Watch%20A%20Channel%20_Special%20008%20online%20in%20high%20quality.mp4?st=vapQu1DhWcD4ql9GgSxbsQ&e=1521869802',
+              '9': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20009%20-%20Watch%20A%20Channel%20Episode%20009%20online%20in%20high%20quality.mp4?st=9E7shB1Y3xjAnJFDA_HgrQ&e=1521869802',
+              '10': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20010%20-%20Watch%20A%20Channel%20_Special%20010%20online%20in%20high%20quality.mp4?st=LSanrMHrrtubUIelCf0dLg&e=1521869802',
+              '11': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20_Special%20011%20-%20Watch%20A%20Channel%20_Special%20011%20online%20in%20high%20quality.mp4?st=MfnhP8sR_6vDYkDsQ0CUgA&e=1521869802',
+              '12': 'https://lb.watchanimesub.net/A%20Channel/A%20Channel%20Episode%20012%20-%20Watch%20A%20Channel%20Episode%20012%20online%20in%20high%20quality.mp4?st=rwb7KmKecwZCyohA51uZFA&e=1521869802'
+            }
+        }
+        ];
